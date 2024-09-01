e7838c5dca33da23a27de50c374e40371793a96a
roy35-909 <roy35-909@diu.edu.bd>
2024-09-01 20:34:34 +0530

    update from server

--- dataplay/settings.py
+++ dataplay/settings.py
@@ -107,24 +107,24 @@ WSGI_APPLICATION = 'dataplay.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 
 
         # On Production Please Comment Out this Code
-    #     'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': env('DATABASE_NAME'),
-    #     'USER': env('DATABASE_USER'),
-    #     'PASSWORD': env('DATABASE_PASS'),
-    #     'HOST': env('DATABASE_IP'),
-    #     'PORT': '3306',
-    #     'OPTIONS' : {
-    #         'charset' : 'utf8mb4'
-    #     }
-    # },
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_IP'),
+        'PORT': '3306',
+        'OPTIONS' : {
+            'charset' : 'utf8mb4'
+        }
+    },
 }
 
 
@@ -213,7 +213,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 REDIRECT_URI = ['https://dataplaynew-fronten-git-d1b1c7-nishant-guptas-projects-241823fa.vercel.app/googleoAuth/','https://www.dataplay.co.in/googleoAuth/','http://localhost:8000/googleoAuth/',
-                'http://localhost:3000/googleoAuth/','http://localhost:8000','https://dataplaynew-frontend-3olobyonk-nishant-guptas-projects-241823fa.vercel.app/googleoAuth/']
+                'http://localhost:3000/googleoAuth/','http://localhost:8000','https://dataplaynew-frontend-3olobyonk-nishant-guptas-projects-241823fa.vercel.app/googleoAuth/','https://preview.dataplay.co.in/googleoAuth/']
 DJOSER = {
     'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {
---
+++ passenger_wsgi.py
@@ -0,0 +1,1 @@
+from dataplay.wsgi import application
